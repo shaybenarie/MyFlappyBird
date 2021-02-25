6799d12c475c28ad11e104982943cd9fee10cd20
shaybenarie <shaybenarie2002@gmail.com>
2021-02-25 19:48:31 +0200

    sort into dirs

---
+++ Assets/Scripts/birdFly.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class birdFly : MonoBehaviour
+{
+    public GameManager manger;
+    public GameObject GOcanvas;
+    public float velocity = 1;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GOcanvas.SetActive(false);
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            rb.velocity = Vector2.up * velocity;
+        }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        manger.GameOver();
+    }
+    
+ 
+}
---
+++ Assets/Scripts/newObs.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newObs : MonoBehaviour
+{
+    public float maxTime = 1;
+    public float Timer = 4;
+    public GameObject Obstacles;
+    public float height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newObs = Instantiate(Obstacles);
+        newObs.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), -0.556804f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Timer > maxTime) {
+            GameObject newObs = Instantiate(Obstacles);
+            newObs.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), -0.556804f);
+            Destroy(newObs, 15);
+            Timer = 0;
+        }
+        Timer += Time.deltaTime;
+    }
+}
